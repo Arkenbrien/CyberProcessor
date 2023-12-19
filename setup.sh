8d864a6928a98e185e4775c35ecda544253b40bb
Trav <tm769613@ohio.edu>
2023-12-19 13:59:15 -0500

    added makefile and setup...

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo apt install -y python3-rosbag python3-pip python3-numpy python3-scipy python3-matplotlib python3-sensor-msgs python3-protobuf protobuf-compiler
+sudo apt install -y 
+sudo pip3 install boto3 pyprog pymongo rospy_message_converter python-dotenv tinydb dynamodb-json
+
+#sudo pip3 install sensor-msgs
+sudo apt install -y python3-sensor-msgs
+sudo apt install -y python3-protobuf
+sudo apt install -y protobuf-compiler
+
+make
