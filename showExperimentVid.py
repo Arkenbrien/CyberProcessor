9ce69ba311e37777688c03058551b1cd9818870a
tm769613@ohio.edu <tm769613@ohio.edu>
2023-12-27 12:40:45 -0500

    updated for metadata

--- showExperimentVid.py
+++ showExperimentVid.py
@@ -25,40 +25,47 @@ def main(video_path, json_file):
     # Loop through frames
     for frame_number in range(frame_count):
         # Read frame
-        # frame_number += 1
-        ret, frame = cap.read()
-        if not ret:
-            print("Error reading frame.")
-            break
+        if frame_number == 0:
+            print("GOT COMMENTS", metadata[frame_number])
+        elif frame_number == 1:
+            print("GOT METADATA", metadata[frame_number])
 
-        if frame_number > 0:
-            # Get metadata for the current frame
-            frame_metadata = metadata[frame_number]
+        else:
+            # frame_number += 2
+            ret, frame = cap.read() 
 
-            # Extract values from metadata
-            chassis = frame_metadata.get('chassis')
-            loco    = frame_metadata.get('localization')
-
-            lat,lon = utm.to_latlon(loco['pose']['position']['x'], loco['pose']['position']['y'], 17, 'S')
-
-            lat = round(lat,5)
-            lon = round(lon,5)
-
-            driving_mode = chassis['drivingMode']
-            speed = round(chassis['speedMps'] * 2.23694, 2)
-
-            # std = loco['']
-
-            # Display values on the screen
-            text = f'Frame: {frame_number}, Lat: {lat}, Lon: {lon}, Driving Mode: {driving_mode}, Speed (mph): {speed}'
-            cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-
-            # Show the frame
-            cv2.imshow('Video with Metadata', frame)
-
-            # Break the loop if the user presses 'q'
-            if cv2.waitKey(int(1000 / fps)) & 0xFF == ord('q'):
+            if not ret:
+                print("Error reading frame.")
                 break
+
+            if frame_number > 0:
+                # Get metadata for the current frame
+                frame_metadata = metadata[frame_number]
+
+                # Extract values from metadata
+                chassis = frame_metadata.get('chassis')
+                loco    = frame_metadata.get('localization')
+
+                lat,lon = utm.to_latlon(loco['pose']['position']['x'], loco['pose']['position']['y'], 17, 'S')
+
+                lat = round(lat,5)
+                lon = round(lon,5)
+
+                driving_mode = chassis['drivingMode']
+                speed = round(chassis['speedMps'] * 2.23694, 2)
+
+                # std = loco['']
+
+                # Display values on the screen
+                text = f'Frame: {frame_number}, Lat: {lat}, Lon: {lon}, Driving Mode: {driving_mode}, Speed (mph): {speed}'
+                cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+                # Show the frame
+                cv2.imshow('Video with Metadata', frame)
+
+                # Break the loop if the user presses 'q'
+                if cv2.waitKey(int(1000 / fps)) & 0xFF == ord('q'):
+                    break
 
     # Release video capture object and close window
     cap.release()
